0b958d8fae893070ba83bb0bb473fadaac6072d3
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-10-18 14:41:16 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
-# Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+   
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+def greet_with_default(name="programmer")
+        puts "Hello, #{name}!"
+end
+
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+sum1 = add(2,2)
+
+def halve (no1)
+    if no1.class !=Integer
+        return nil
+    end
+   no1/2
+end
